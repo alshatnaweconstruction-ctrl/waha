cfd167b559cc2712f3607e8ed8b7c657081eb043
alshatnaweconstruction-ctrl <alshatnawe.construction@gmail.com>
2026-08-28 14:22:18 +0300

    feat: sync world-class architectural platform with Space Grotesk + Inter and BuildReady AI simulator

---
+++ assets/index-DMoPFA8f.js
@@ -0,0 +1,1 @@
+import{r as o,R as s}from"./vendor-motion-DTZY4Nmb.js";import{W as u}from"./index-zcT8AF6B.js";var i=s[" useId ".trim().toString()]||(()=>{}),c=0;function f(r){const[t,e]=o.useState(i());return u(()=>{e(a=>a??String(c++))},[r]),t?`radix-${t}`:""}export{f as u};
